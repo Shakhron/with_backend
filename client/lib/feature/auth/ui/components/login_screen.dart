71c918d9380363622782743e44eaa662e2cee625
Shakhron <sokers30@gmail.com>
2023-03-25 17:37:49 +0300

    add login screen

--- client/lib/feature/auth/ui/components/login_screen.dart
+++ client/lib/feature/auth/ui/components/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
+
+  final controllerLogin = TextEditingController();
+  final controllerPassword = TextEditingController();
+  final GlobalKey<FormState> globalKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +13,55 @@ class LoginScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Login Screen"),
       ),
+      body: Form(
+        key: globalKey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  validator: emptyValodator,
+                  maxLength: 1,
+                  controller: controllerLogin,
+                  decoration: const InputDecoration(
+                    labelText: "Логин",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  validator: emptyValodator,
+                  maxLength: 1,
+                  controller: controllerPassword,
+                  decoration: const InputDecoration(
+                    labelText: "Пароль",
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {
+                    if (globalKey.currentState?.validate() == true) {}
+                  },
+                  style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all<Size>(
+                          const Size(double.maxFinite, 40))),
+                  child: const Text("Войти"),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  String? emptyValodator(String? value) {
+    if (value!.isEmpty) {
+      return "Обязательное полу";
+    }
+    return null;
   }
 }
